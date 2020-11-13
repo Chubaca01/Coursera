1d02e1c77ce91cf0d3198629c3f059289d9a3566
jpruster <jpruster@yahoo.com>
2020-11-12 16:56:34 -0800

    adding codo outline and documentation for CM! assesment

--- stats.c
+++ stats.c
@@ -2,20 +2,21 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief Functions that analyze an array fo char
  *
- * <Add Extended Description Here>
+ * Functions that can analyze an array of unsigned char data items and report
+ * analytics on the maximum, minimum, mean, and median of the data set.
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author JP Ruster
+ * @date 11/12/2020
  *
  */
 
@@ -41,3 +42,29 @@ void main() {
 }
 
 /* Add other Implementation File Code Here */
+void sort_array(unsigned char *array, unsigned int arraySize) {
+
+};
+
+unsigned char find_minimum(unsigned char *array, unsigned int arraySize) {
+
+};
+
+unsigned char find_maximum(unsigned char *array, unsigned int arraySize) {
+
+};
+
+unsigned char find_mean(unsigned char *array, unsigned int arraySize) {
+
+};
+
+unsigned char find_median(unsigned char *array,unsigned int arraySize ) {
+
+};
+
+void print_statistics(unsigned char* array,unsigned int arraySize){
+};
+
+void print_array(unsigned char* array,unsigned int arraySize){
+
+};
